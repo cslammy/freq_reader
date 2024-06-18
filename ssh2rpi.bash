7aa894c2b9ce016566cd2c944899cea373315403
cslammy <audiodiwhy@gmail.com>
2024-06-18 14:56:17 -0700

    initial upload

---
+++ ssh2rpi.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+# connect to pi
+sudo ssh pi@192.168.4.200
---
+++ uf2copy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp '/home/clamm/Dropbox/raspberry pico/PICO-VSCODE-PROJECTS/freq_reader/build/picofreq.uf2'  /media/clamm/RPI-RP2/.
+
